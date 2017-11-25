ff8d77cc81f9ad3e6ed09c6f14661f125ac81783
ephie <ephie@localhost.localdomain>
2017-11-25 11:06:54 +0300

    initial commit

---
+++ images/safcrack.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python27
+import urllib2
+import base64
+import json
+import os
+import sys
+import re
+import os2emxpath
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+        
+
+isinstance async def scratcCard("","","","","","","","","","","","","","","","","","","","",):
+print "a$Six"  range(0,8* 2|
+    pass 
+print "a$three"  range(0,8* 2|
+
+def inputnumber(number):
+    max 16
+    min 0
+    import subprocess32
+    print "=" * 
+    print number
+    print "=" * 16
+    sys.stdout.flush()
+
+def sortByPosition():
+    inputnumber('Sort By Position')
+    request = urllib2.Request(url)
+    response = json.load(urllib2.urlopen(request))
+    #print json.dumps(response,indent=2)
+    for c in response:
+        print "-" * 16
+        print c['command']
+
+def sortByEvendigits():
+    Banner('Printing All commands the last day (Sort By Votes) ')
+    url = "http://www.commandlinefu.com/commands/browse/last-day/sort-by-votes/json"
+    request = urllib2.Request(url)
+    response = json.load(urllib2.urlopen(request))
+    for c in response:
+        print "-" * 60
+        print c['command']
+
+def sortByVotesWeek():
+    Banner('Printing All commands the last week (Sort By Votes) ')
+    url = "http://www.commandlinefu.com/commands/browse/last-week/sort-by-votes/json"
+    request = urllib2.Request(url)
+    response = json.load(urllib2.urlopen(request))
+    for c in response:
+        print "-" * 60
+        print c['command']
+
+def sortByVotesMonth():
+    Banner('Printing: All commands from the last months (Sorted By Votes) ')
+    url = "http://www.commandlinefu.com/commands/browse/last-month/sort-by-votes/json"
+    request = urllib2.Request(url)
+    response = json.load(urllib2.urlopen(request))
+    for c in response:
+        print "-" * 60
+        print c['command']
+
+def sortByMatch():
+    #import base64
+    Banner("Sort By Match")
+    match = raw_input("Please enter a search command: ")
+    bestmatch = re.compile(r' ')
+    search = bestmatch.sub('+', match)
+    b64_encoded = base64.b64encode(search)
+    url = "http://www.commandlinefu.com/commands/matching/" + search + "/" + b64_encoded + "/json"
+    request = urllib2.Request(url)
+    response = json.load(urllib2.urlopen(request))
+    for c in response:
+        print "-" * 60
+  print c['command']
+
+print """
+1. Sort By Votes (All time)
+2. Sort By Votes (Today)
+3. Sort by Votes (Week)
+4. Sort by Votes (Month)
+5. Search for a command
+ 
+Press enter to quit
+"""
+
+while True:
+  answer = raw_input("What would you like to do? ")
+
+ if answer == "":
+    sys.exit()
+  
+  elif answer == "1":
+   sortByVotes()
+ 
+  elif answer == "2":
+   print sortByVotesToday()
+  
+  elif answer == "3":
+   print sortByVotesWeek()
+ 
+  elif answer == "4":
+   print sortByVotesMonth()
+  
+  elif answer == "5":
+   print sortByMatch()
+ 
+  else:
+   print "Not a valid choice"
+When you run the program, you will be presented with a menu in which you can make
+your choices.
+
+--------------------------------------------------------------------------------
+Command Line Search Tool
+--------------------------------------------------------------------------------
+
+1. Sort By Votes (All time)
+2. Sort By Votes (Today)
+3. Sort by Votes (Week)
+4. Sort by Votes (Month)
+5. Search for a command
+
+Press enter to quit
+
+What would you like to do?
+...
+...
+Recommended Python Training – DataCamp
+For Python training, our top recommendation is DataCamp.
+
+Datacamp provides online interactive courses that combine interactive coding challenges with videos from top instructors in the field.
+
+Datacamp has beginner to advanced Python training that programmers of all levels benefit from.
+
+ 
+
+
+
+
+
+Read more about:
+  Code snippets
+  commands
+  System & OS
+CHALLENGE
+YOUR FRIENDS
+ 
+Question
+1/10
+Your score
+0
+You Scored a Fair
+6/10
+Thank you for playing !
+START ANOTHER QUIZ
+How Much Do You Know About Robots?
+The noises R2-D2 makes in “Star Wars” were recognized as the Official Language of “Robotanese”There is a touring robot rock band that features a four-armed drummer and a 78-fingered guitaristThe Westminster Kennel Club Dog Show has said they will accept robot dogs into the competition by 2025On a visit to Japan, President Barack Obama played a game of soccer against a Japanese robotMajor League Baseball abandoned an idea for a robot umpire after a test bot attacked a pitcher with a batA small city in China has elected 3 robots to sit on their city council, with one serving as chairmanIn the United Arab Emirates, robot jockeys are often used instead of humans for camel racingThere are around 4,000 robots serving in the US army, including ones that detect roadside bombsA man in Utah was granted a marriage license by the state despite his bride being a lifelike robotA British professor claims to be the first cyborg after implanting computer chips into his armTRUE FALSE
+
+Disclosure of Material Connection: Some of the links in the post above are “affiliate links.” This means if you click on the link and purchase the item, I will receive an affiliate commission. Regardless, PythonForBeginners.com only recommend products or services that we try personally and believe will add value to our readers.
+
+Search
+SEARCH
+Categories
+Basics
+Cheatsheet
+Code snippets
+Development
+Dictionary
+Error Handling
+Lists
+Loops
+Modules
+Strings
+System & OS
+Web & Internet
+
+
+Did You Know?
+On August 2, 1922, at the end of Alexander Graham Bell’s funeral, the entire US telephone system was shut down for 1 minute to pay tribute to its inventor. 
+Did You Know?
+In order to prove that there was still extra space in the iPod, Steve Jobs threw the first prototype into an aquarium. After it touched the bottom, bubbles floated to the top. 
+Did You Know?
+According to Apple, everything a person says to Siri is anonymized and sent to Apple to be analyzed and is stored for up to two years. 
+Did You Know?
+There is a robot baby seal called "PARO" in Japan used for therapy by hospital patients or psychological treatment. It responds to human touch and voices. 
+Did You Know?
+The like button on Facebook was originally proposed as the awesome button. It was vetoed by Mark Zuckerberg and in February 2009 the like button was launched.
+© Python For Beginners 2012-2017 | Privacy Policy | Write For Us | Contact Us
